5d7c1b7e2753f7a97c33550119277f939f4b628e
dtucker <davidt2003@msn.com>
2025-10-18 18:34:33 -0400

    feat: add submitter entity class

---
+++ app/Models/Account/Entities/Submitter.php
@@ -0,0 +1,18 @@
+<?php namespace Models\Account\Entities;
+
+use Zephyrus\Core\Entity\Entity;
+
+class Submitter extends Entity
+{
+    public string $address;
+    public string $level;
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $jurisdiction;
+    public string $profile_hash;
+    public ?int $organization_id;
+    public ?string $verification_token;
+    public string $created_at;
+    public string $updated_at;
+}
