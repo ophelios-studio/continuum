fc62cc4134c099bc3fb9c0e4c36a179c711f3a7e
dtucker <davidt2003@msn.com>
2025-10-18 12:45:13 -0400

    feat: add wallet format

--- app/formats.php
+++ app/formats.php
@@ -13,3 +13,7 @@ Formatter::register('day_full', function ($dateTime) {
     $formatter = new IntlDateFormatter(Locale::getDefault(), IntlDateFormatter::LONG, IntlDateFormatter::SHORT, null, null, "EEEE");
     return $formatter->format($dateTime->getTimestamp());
 });
+
+Formatter::register('wallet', function (string $wallet) {
+    return substr($wallet, 0, 4) . "..." . substr($wallet, -4);
+});
