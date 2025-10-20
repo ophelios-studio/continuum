740e2cbabcee30e49f437a2ea480b92dc0cecfb9
dtucker <davidt2003@msn.com>
2025-10-20 10:00:44 -0400

    feat: default workflow for actor registration

--- app/Models/Account/Services/ActorService.php
+++ app/Models/Account/Services/ActorService.php
@@ -2,7 +2,9 @@
 
 use Models\Account\Brokers\ActorBroker;
 use Models\Account\Entities\Actor;
+use Models\Account\Validators\ActorValidator;
 use Zephyrus\Application\Form;
+use Zephyrus\Core\Session;
 
 readonly class ActorService
 {
@@ -17,6 +19,11 @@ readonly class ActorService
 
     public function insert(Form $form): Actor
     {
-
+        ActorValidator::assertInsert($form);
+        $new = $form->buildObject();
+        $new->address = Session::get('wallet');
+        $verificationToken = $this->broker->insert($new);
+        // TODO: send email verification
+        return $this->findByAddress($new->address);
     }
 }
