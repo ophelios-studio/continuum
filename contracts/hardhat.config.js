ec6ab8911a78012989b80afb90476b39783665ba
dtucker <davidt2003@msn.com>
2025-10-20 09:41:26 -0400

    chore: remove tests and typescript for HardHat project

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import "@nomicfoundation/hardhat-ignition";
+
+const PK = process.env.DEPLOYER_PK ? [process.env.DEPLOYER_PK] : [];
+
+export default {
+    solidity: {
+        version: "0.8.28",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: process.env.SEPOLIA_RPC || "https://rpc.sepolia.org",
+            chainType: "l1",
+            accounts: PK,
+            chainId: 11155111
+        }
+    }
+};
