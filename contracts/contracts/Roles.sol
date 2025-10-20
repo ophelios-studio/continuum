e8dc43cf327257b3834c7793bf6ce1c8847a9262
dtucker <davidt2003@msn.com>
2025-10-20 14:56:04 -0400

    feat: add role level for submitter

--- contracts/contracts/Roles.sol
+++ contracts/contracts/Roles.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.28;
 library Roles {
     bytes32 public constant ADMIN = keccak256("ADMIN");
     bytes32 public constant VALIDATOR = keccak256("VALIDATOR");
+    bytes32 public constant ORG_ADMIN = keccak256("ORG_ADMIN");
+    bytes32 public constant UPGRADER = keccak256("UPGRADER");
+
+    enum RoleLevel { NONE, DECLARED, VERIFIED_L1, VERIFIED_L2, REVOKED }
 }
