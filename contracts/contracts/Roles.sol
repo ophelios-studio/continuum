74d951437b9542cf9f1041a1f448a56a980e99b5
dtucker <davidt2003@msn.com>
2025-10-20 14:54:28 -0400

    feat: add validator role for contracts

--- contracts/contracts/Roles.sol
+++ contracts/contracts/Roles.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.8.28;
 
 library Roles {
     bytes32 public constant ADMIN = keccak256("ADMIN");
-
-    enum RoleLevel { NONE, DECLARED, VERIFIED_L1, VERIFIED_L2, REVOKED }
+    bytes32 public constant VALIDATOR = keccak256("VALIDATOR");
 }
